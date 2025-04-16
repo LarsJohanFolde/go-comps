05c4a76fb0d621a49635703281611be7c9f50ffe
Lars Johan Folde <ljfolde@gmail.com>
2025-04-16 18:11:45 +0200

    Renamed DB fields in queries
    The WCA Devloper database updated their table and column names to
    provide consistency between all tables. All tables and columns now use
    snake case.

--- db/db.go
+++ db/db.go
@@ -29,7 +29,7 @@ func GetPersons() []models.Person {
 	}
 	defer db.Close()
 
-	rows, err := db.Query("SELECT wca_id, name, countryId FROM Persons WHERE subId = '1'")
+	rows, err := db.Query("SELECT wca_id, name, country_id FROM persons WHERE sub_id = '1'")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -59,18 +59,18 @@ func GetUpcomingCompetitions(wcaId string) []models.Competition {
 	query := fmt.Sprintf(`
     SELECT
         c.name AS Competition,
-        c.countryid AS CountryId,
+        c.country_id AS CountryId,
         c.start_date AS StartDate,
         c.end_date AS EndDate,
         COALESCE((SELECT r1.competing_status FROM registrations r1 WHERE r1.competition_id = c.id AND r1.user_id = (
         SELECT id from users WHERE wca_id = '%s'
         )), "accepted"),
         CASE WHEN start_date > CURRENT_DATE() THEN true ELSE false END AS Upcoming
-    FROM Competitions c
+    FROM competitions c
     WHERE c.id IN (
             SELECT r.competition_id
             FROM registrations r
-            JOIN Competitions c1 ON r.competition_id = c.id
+            JOIN competitions c1 ON r.competition_id = c.id
             WHERE r.user_id = (SELECT id FROM users WHERE wca_id = '%s')
 		   )
     GROUP BY c.id
